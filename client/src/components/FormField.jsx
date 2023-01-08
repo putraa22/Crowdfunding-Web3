71346d8961f1fcbb9ba063784d22b93e325818ea
Hermansyah Putra <hermansyahputra0418@gmail.com>
2023-01-08 12:19:54 +0700

    Create Campaign & Setup Context, & Connect Wallet

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -16,16 +16,23 @@ const FormField = ({
         </span>
       )}
       {isTextArea ? (
-        <textarea />
+        <textarea
+          required
+          value={value}
+          onChange={handleChange}
+          rows={10}
+          placeholder={placeholder}
+          className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
+        />
       ) : (
-        <input 
-            required
-            value={value}
-            onChange={handleChange}
-            type={inputType}
-            step="0.1"
-            placeholder={placeholder}
-            className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
+        <input
+          required
+          value={value}
+          onChange={handleChange}
+          type={inputType}
+          step="0.1"
+          placeholder={placeholder}
+          className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
         />
       )}
     </label>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
 import App from "./App";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThirdwebProvider desiredChainId={ChainId.Goerli}>
     <Router>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </Router>
   </ThirdwebProvider>
 );
